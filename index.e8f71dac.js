9c7315cd37560e754fa5270092e009bbef648677
z3rt <z3rt@MacBook-Pro-Aleksandr.local>
2025-05-26 21:59:02 +0300

    make build

---
+++ index.e8f71dac.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector(".gallery").addEventListener("click",function(r){r.preventDefault();var t=r.target.closest("a","img");t&&(e.src=t.href)});
+//# sourceMappingURL=index.e8f71dac.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const mainImg = document.querySelector("#largeImg");
+const pickImg = document.querySelector(".gallery");
+pickImg.addEventListener("click", (imgChange)=>{
+    imgChange.preventDefault();
+    const link = imgChange.target.closest("a");
+    if (!link) return;
+    mainImg.src = link.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
